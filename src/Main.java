11f7457913342e38f0f7fdfb88d8572e4d4f1c48
Rabbicik <rabbicikpl@gmail.com>
2023-09-18 14:57:23 +0200

    nowy projekt

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        List<pojazdy> pojazdyList = new ArrayList<>();
-        pojazdyList.add(new auta());
-        pojazdyList.add(new motory());
-
-        for (pojazdy pojazdy : pojazdyList) {
-            pojazdy.activaceBreake();
-        }
-    }
 }
